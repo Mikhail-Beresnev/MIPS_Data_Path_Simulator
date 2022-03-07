bb71acbb9d59027c84237048d2b7d7bd7ae46daf
Guest <guest-muteww@csl08.cs.wallawalla.edu>
2022-03-06 16:33:35 -0800

    adding script

--- MEM/mem.cpp
+++
@@ -1,7 +0,0 @@
-#include "mem.h"
-
-string left_shift_2(string input) {
-    // input is 32 bits
-
-    return input.substr(0, 29) + "00";
-}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 // #include "FILENAME.H"
+#include "./MEM/mem.h"
 
+using namespace std;
 
 int main() {
-
-
+    cout << "test" << endl;
 }
