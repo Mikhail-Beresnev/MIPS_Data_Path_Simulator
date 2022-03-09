5050fdb45e37323622d237e89df86faf3423c132
Mikhail Beresnev <MBeresnev01@gmail.com>
2022-03-08 20:13:16 -0800

    reintroduced compile script

--- IF/if.cpp
+++ IF/if.cpp
@@ -1,40 +1,43 @@
-#include if.h
+#include "if.h"
 
 //  Asks the User to Input an Instruction in Binary Format and checks if it is binary
-string PC(){
-bool binary; // flag if number is not binary
-do{
-cout << "Enter an Intruction in 32-bit binary format: ";
-cin >> Instruction; 
-int n = Instruction.length();
-char x[n + 1];
- // copying the contents of the string to char array
-strcpy(x, Instruction.c_str());
+string PC() {
+    bool binary; // flag if number is not binary
+    do{
+        string instruction;
+        cout << "Enter an Intruction in 32-bit binary format: ";
+        cin >> instruction; 
+        // int n = instruction.length();
+        const int n = 0;
+        char x[n + 1];
+        // copying the contents of the string to char array
+        strcpy(x, instruction.c_str());
  
-    for (int i = 0; i < n; i++){
-        if(x[i] != 0 && x[i] != 1){
-binary = false;
-        }
-    }
-
-} while(n!=32 || binary = false)
+        for (int i = 0; i < n; i++){
+            if(x[i] != 0 && x[i] != 1){
+            binary = false;
+            }   
+        }   
+    } while (true);
+    // while(n!=32 || binary = false);
+    return "0";
 }
 
 //  Asks the User to Input an Instruction in Binary Format
 string intructionMemory(string input){
-
+    return "0";
 
 }
 
 // Asks the User to Input an Instruction in Binary Format
 string Adder(string inputA, string inputB){
-
+    return "0";
 
 
 }
 
 // Asks the User to Input an Instruction in Binary Format
 string Mux(string input){
-
+    return "0";
 
 }
---
+++ helpful/helpful.cpp
@@ -0,0 +1,26 @@
+#include "helpful.h"
+
+string add_binary(string input_1, string input_2) {
+
+    unsigned long x = bitset<32>(input_1).to_ulong();
+    unsigned long y = bitset<32>(input_2).to_ulong();
+
+    unsigned long result = x + y;
+
+    string output = bitset<32>(result).to_string();
+
+    return output;
+}
+
+
+string sub_binary(string input_1, string input_2) {
+
+    unsigned long x = bitset<32>(input_1).to_ulong();
+    unsigned long y = bitset<32>(input_2).to_ulong();
+
+    unsigned long result = x - y;
+
+    string output = bitset<32>(result).to_string();
+
+    return output;
+}
