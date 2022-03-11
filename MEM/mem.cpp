3c6a951a383cc0ee02971ada375122105b8d5ee0
Mikhail Beresnev <MBeresnev01@gmail.com>
2022-03-10 23:19:32 -0800

    fix compile script

--- MEM/mem.cpp
+++ MEM/mem.cpp
@@ -1,16 +1,16 @@
 #include "mem.h"
 
-string data_memory(string write_data, string address, string mem_write, string mem_read) {
-    if (mem_write == mem_read) {
-        // error
-        return "ERROR";
-    }
-    if (mem_write == "1") {
-        data_memory.insert(make_pair(address, write_data));
-        // return nothing
-    }
+// string data_memory(string write_data, string address, string mem_write, string mem_read) {
+//     if (mem_write == mem_read) {
+//         // error
+//         return "ERROR";
+//     }
+//     if (mem_write == "1") {
+//         data_memory.insert(make_pair(address, write_data));
+//         // return nothing
+//     }
 
-    if (mem_read == "1") {
-        return data_memory.at(address);
-    }
-}
+//     if (mem_read == "1") {
+//         return data_memory.at(address);
+//     }
+// }
